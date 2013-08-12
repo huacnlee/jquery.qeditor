9ad5032c0c9e5ac9e7b23804fb9125a0131bd0cb
Jason Lee <huacnlee@gmail.com>
2013-08-12 21:49:04 +0800

    -

--- js/jquery.qeditor.js
+++ js/jquery.qeditor.js
@@ -26,7 +26,7 @@ In Rails application, you can use like this:
 
 var QEDITOR_ALLOW_TAGS_ON_PASTE, QEDITOR_DISABLE_ATTRIBUTES_ON_PASTE, QEDITOR_TOOLBAR_HTML;
 
-QEDITOR_TOOLBAR_HTML = "<div class=\"qeditor_toolbar\">\n  <a href=\"#\" onclick=\"return QEditor.action(this,'bold');\" title=\"Bold\"><span class=\"icon-bold\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'italic');\" title=\"Italic\"><span class=\"icon-italic\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'underline');\" title=\"Underline\"><span class=\"icon-underline\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'strikethrough');\" title=\"StrikeThrough\"><span class=\"icon-strikethrough\"></span></a>		 \n  <span class=\"vline\"></span> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertorderedlist');\"><span class=\"icon-list-ol\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertunorderedlist');\"><span class=\"icon-list-ul\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'indent')\"><span class=\"icon-indent-right\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'outdent')\"><span class=\"icon-indent-left\"></span></a> \n  <span class=\"vline\"></span> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertHorizontalRule');\"><span class=\"icon-minus\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'formatBlock','blockquote');\"><span class=\"icon-quote-left\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'formatBlock','PRE');\"><span class=\"icon-code\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'createLink');\"><span class=\"icon-link\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertimage');\"><span class=\"icon-picture\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.toggleFullScreen(this);\" class=\"pull-right\"><span class=\"icon-fullscreen\"></span></a> \n</div>";
+QEDITOR_TOOLBAR_HTML = "<div class=\"qeditor_toolbar\">\n  <a href=\"#\" onclick=\"return QEditor.action(this,'bold');\" title=\"Bold\"><span class=\"icon-bold\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'italic');\" title=\"Italic\"><span class=\"icon-italic\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'underline');\" title=\"Underline\"><span class=\"icon-underline\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'strikethrough');\" title=\"StrikeThrough\"><span class=\"icon-strikethrough\"></span></a>		 \n  <span class=\"vline\"></span> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertorderedlist');\"><span class=\"icon-list-ol\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertunorderedlist');\"><span class=\"icon-list-ul\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'indent')\"><span class=\"icon-indent-right\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'outdent')\"><span class=\"icon-indent-left\"></span></a> \n  <span class=\"vline\"></span> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertHorizontalRule');\"><span class=\"icon-minus\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'formatBlock','blockquote');\"><span class=\"icon-quote-left\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'formatBlock','PRE');\"><span class=\"icon-code\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'createLink');\"><span class=\"icon-link\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.action(this,'insertimage');\"><span class=\"icon-picture\"></span></a> \n  <a href=\"#\" onclick=\"return QEditor.toggleFullScreen(this);\" class=\"qeditor_fullscreen_button pull-right\"><span class=\"icon-fullscreen\"></span></a> \n</div>";
 
 QEDITOR_ALLOW_TAGS_ON_PASTE = "div,p,ul,ol,li,hr,br,b,strong,i,em,img,h2,h3,h4,h5,h6,h7";
 
@@ -69,15 +69,23 @@ window.QEditor = {
     }
   },
   toggleFullScreen: function(el) {
-    var editor;
+    var border;
 
-    editor = $(el).parent().parent();
-    if (editor.data("qe-fullscreen") === "1") {
-      editor.css("width", editor.data("qe-width")).css("height", editor.data("qe-height")).css("top", editor.data("qe-top")).css("left", editor.data("qe-left")).css("position", "static").css("z-index", 0).data("qe-fullscreen", "0");
+    border = $(el).parent().parent();
+    if (border.data("qe-fullscreen") === "1") {
+      QEditor.exitFullScreen();
     } else {
-      editor.data("qe-width", editor.width()).data("qe-height", editor.height()).data("qe-top", editor.position().top).data("qe-left", editor.position().left).data("qe-fullscreen", "1").css("top", 0).css("left", 0).css("position", "absolute").css("z-index", 99999).css("width", $(window).width()).css("height", $(window).height());
+      QEditor.enterFullScreen(border);
     }
     return false;
+  },
+  enterFullScreen: function(border) {
+    border.data("qe-fullscreen", "1").addClass("qeditor_fullscreen");
+    border.find(".qeditor_preview").focus();
+    return border.find(".qeditor_fullscreen_button span").attr("class", "icon-resize-small");
+  },
+  exitFullScreen: function() {
+    return $(".qeditor_border").removeClass("qeditor_fullscreen").data("qe-fullscreen", "0").find(".qeditor_fullscreen_button span").attr("class", "icon-fullscreen");
   },
   renderToolbar: function(el) {
     return el.parent().prepend(QEDITOR_TOOLBAR_HTML);
@@ -100,7 +108,7 @@ window.QEditor = {
       });
     } else {
       return this.each(function() {
-        var currentVal, editor, hidden_flag, obj;
+        var currentVal, editor, hidden_flag, obj, placeholder;
 
         obj = $(this);
         obj.addClass("qeditor");
@@ -109,11 +117,31 @@ window.QEditor = {
           obj.after(hidden_flag);
         } else {
           editor = $('<div class="qeditor_preview clearfix" style="overflow:scroll;" contentEditable="true"></div>');
+          placeholder = $('<div class="qeditor_placeholder"></div>');
+          $(document).keyup(function(e) {
+            if (e.keyCode === 27) {
+              return QEditor.exitFullScreen();
+            }
+          });
           document.execCommand('defaultParagraphSeparator', false, 'p');
           currentVal = obj.val();
           editor.html(currentVal);
           editor.addClass(obj.attr("class"));
           obj.after(editor);
+          placeholder.text(obj.attr("placeholder"));
+          editor.attr("placeholder", obj.attr("placeholder"));
+          editor.append(placeholder);
+          editor.focusin(function() {
+            return $(this).find(".qeditor_placeholder").remove();
+          });
+          editor.blur(function() {
+            var t;
+
+            t = $(this);
+            if (t.text().length === 0) {
+              return $(this).html('<div class="qeditor_placeholder">' + $(this).attr("placeholder") + '</div>');
+            }
+          });
           editor.change(function() {
             var pobj, t;
 
